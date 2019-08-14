7922b5378b665f8ef590a6dc22d9246afc0d5958
Lena Maglo <maglo.lena@gmail.com>
2019-08-14 17:17:28 +0200

    my airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do |flat|
+  flat = Flat.new
+  flat.name = Faker::Hipster.sentence
+  flat.address = Faker::Address.full_address
+  flat.description = Faker::Hipster.sentences
+  flat.price_per_night = rand(100)
+  flat.number_of_guests = rand(10)
+  flat.save!
+end
